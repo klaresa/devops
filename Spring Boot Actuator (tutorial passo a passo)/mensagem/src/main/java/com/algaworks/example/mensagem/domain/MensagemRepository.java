4c8edddc882a016646bdb541122fc27aa98d9ce6
klari <klaritatis@gmail.com>
2023-04-13 10:40:54 -0300

    changes

--- Spring Boot Actuator (tutorial passo a passo)/mensagem/src/main/java/com/algaworks/example/mensagem/domain/MensagemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.example.mensagem.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
-
-}
